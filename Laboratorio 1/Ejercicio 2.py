a5e686772dc33931bcac2fb66e06c98585baa609
Ces_LF <javier.sosarnd@uanl.edu.mx>
2024-08-24 09:25:45 -0700

    conf

--- Laboratorio 1/Ejercicio 2.py
+++ Laboratorio 1/Ejercicio 2.py
@@ -1,8 +1,14 @@
-def max_de_tres(num1,num2,num3):
-    if num1>num2 and num1>num3:
-        return num1
-    elif num2>num1 and num2>num3:
-        return num2
-    elif num3>num1 and num3>num2:
-        return num3
-print(max_de_tres(15,30,10))    
+c=input("escribe el primer número: ")
+c=float(c)
+b=input("escribe el segundo número: ")
+b=float(b)
+d=input("escribe el tercer número: ")
+d=float(d)
+def max_de_tres(c,b,d):
+    if c>b and c>d:
+        return c
+    elif b>c and b>d:
+        return b
+    elif d>c and d>b:
+        return d
+print("el mayor es: ",max_de_tres(c,b,d))    
