e7c4e1a51bcbf730a99ccebd59b03fb2ff5eee18
Ces_LF <javier.sosarnd@uanl.edu.mx>
2024-08-26 23:54:34 -0600

    BME

---
+++ main.js
@@ -0,0 +1,6 @@
+function insertartabla(){
+    let tabla='<table>'
+    tabla=tabla+'<tr> <th>#</th> <th>dato o reacción</th></tr>';
+    tabla=tabla+'</table>'
+    document.getElementById('Información adicional').innerHTML=tabla;
+}
