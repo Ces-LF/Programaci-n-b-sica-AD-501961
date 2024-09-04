b98ec27eb184aaf35be4276321286567278561f7
Ces-LF <javier.sosarnd@uanl.edu.mx>
2024-09-04 14:53:47 -0600

    Create Clases.py
    Notas

---
+++ Clases.py
@@ -0,0 +1,15 @@
+#Clases
+#a.Declaración
+class vehículo:
+   def__init__(self):    
+   self.Fabricante
+   self.Anio 
+   self.Ruedas
+   self.Volante
+   self.Motor
+   self.Clima
+   self.Kilometraje
+   self.Puertas
+
+
+   
